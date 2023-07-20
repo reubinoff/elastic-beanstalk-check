66b88430df6040eeeb7878c063d86948ab512031
Moshe Reubinoff <moshe@ownbackup.com>
2023-07-20 11:59:49 +0200

    adding tests

--- action.py
+++ action.py
@@ -1,21 +1,33 @@
+"""
+This module contains functions for interacting with Elastic Beanstalk environments.
+"""
+
 import os
-from boto3.session import Session
 import sys
 import time
 from dataclasses import dataclass
+from boto3.session import Session
+
 
 READY_STATUS = "Ready"
 
+
 @dataclass
 class EnvironmentStatus:
+    """
+    Dataclass for storing environment status"""
     version_label: str
     status: str
     health_status: str
 
     def __str__(self) -> str:
-        return f"EnvironmentStatus(version_label={self.version_label}, status={self.status}, health_status={self.health_status})"
+        return f"""EnvironmentStatus(version_label={self.version_label},
+                 status={self.status}, health_status={self.health_status})"""
+
 
 def get_environment_version(env_name, client) -> EnvironmentStatus:
+    """
+    Get environment version from Elastic Beanstalk"""
     response = client.describe_environments(
         EnvironmentNames=[
             env_name,
@@ -31,15 +43,20 @@ def get_environment_version(env_name, client) -> EnvironmentStatus:
     print(str(env_status))
     return env_status
 
-def is_env_ready(env_status: EnvironmentStatus , app_version_label: str) -> bool:
+
+def is_env_ready(env_status: EnvironmentStatus, app_version_label: str) -> bool:
+    """
+    Check if environment is ready"""
     version_is_ok = env_status.version_label == app_version_label and app_version_label is not None
     if app_version_label is None:
-        version_is_ok = True # no version check required
+        version_is_ok = True    # no version check required
     status_is_ok = env_status.status == READY_STATUS
     return version_is_ok and status_is_ok
 
 
 def main():
+    """
+    Main function for action"""
     region = os.environ['INPUT_REGION']
     timeout = int(os.environ.get('INPUT_TIMEOUT', '60'))
     env_name = os.environ['INPUT_ENV_NAME']
@@ -53,21 +70,19 @@ def main():
     env_status = get_environment_version(env_name, client)
     while is_env_ready(env_status, app_version_label) is False and (time.time() - start) < timeout:
         time.sleep(5)
-        label, status = get_environment_version(env_name, region)
+        env_status = get_environment_version(env_name, region)
 
     print(f'::set-output name=health-status::{env_status.health_status}')
     print(f'::set-output name=version-label::{env_status.version_label}')
     print(f'::set-output name=status::{env_status.status}')
 
     if is_env_ready(env_status, app_version_label) is False:
-        print(f"Fail. Expected version {app_version_label} but got {label}. Status: {status}")
+        print(f"""Fail. Expected version {app_version_label}
+               but got {env_status.version_label}. Status: {env_status.status}""")
         sys.exit(1)
 
     sys.exit(0)
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
--- test_get_environment_version.py
+++ test_get_environment_version.py
@@ -1,22 +1,49 @@
+"""
+Test class for get_environment_version function"""
+
 import unittest
 from unittest.mock import patch
-from action import get_environment_version
+from action import get_environment_version, EnvironmentStatus, READY_STATUS
+
 
 class TestGetEnvironmentVersion(unittest.TestCase):
-    @patch('action.boto3.client')
-    def test_get_environment_version(self, mock_client):
+    """
+    Test class for get_environment_version function"""
+    @patch('boto3.session.Session')
+    def test_get_environment_version_expected(self, mock_client):
+        """
+        Test get_environment_version function with expected response"""
         mock_eb = mock_client.return_value
         mock_eb.describe_environments.return_value = {
             'Environments': [
                 {
-                    'VersionLabel': 'v1.0.0'
+                    'VersionLabel': 'v1.0.0',
+                    'Status': 'Ready',
                 }
             ]
         }
         env_name = 'my-environment'
         expected_version = 'v1.0.0'
-        actual_version = get_environment_version(env_name)
-        self.assertEqual(actual_version, expected_version)
+        env_status: EnvironmentStatus = get_environment_version(env_name, client=mock_eb)
+        self.assertEqual(env_status.version_label, expected_version)
+        self.assertEqual(env_status.status, READY_STATUS)
+        mock_eb.describe_environments.assert_called_once_with(
+            EnvironmentNames=[env_name]
+        )
+
+    @patch('boto3.session.Session')
+    def test_get_environment_version_not_found(self, mock_client):
+        """
+        Test get_environment_version function with environment not found
+        """
+        mock_eb = mock_client.return_value
+        mock_eb.describe_environments.return_value = {
+            'Environments': []
+        }
+        env_name = 'my-environment'
+        with self.assertRaises(SystemExit) as assert_exit:
+            get_environment_version(env_name, client=mock_eb)
+        self.assertEqual(assert_exit.exception.code, 1)
         mock_eb.describe_environments.assert_called_once_with(
             EnvironmentNames=[env_name]
         )
